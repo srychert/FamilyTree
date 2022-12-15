6c605cdcb708c74b0ac2b5cf14cfea8d1653ae57
SebastianRychert <sebastian_rychert@wp.pl>
2022-12-15 14:42:46 +0100

    fix import

--- lab11/src/components/HelloWorld.vue
+++ lab11/src/components/HelloWorld.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <h1>{{ name }}</h1>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
+  props: {
+    msg: String,
+  },
 };
 </script>
 
