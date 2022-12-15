c676f7affb75dc01ef6a7770e7cdd2381de2ca6c
SebastianRychert <sebastian_rychert@wp.pl>
2022-12-15 14:40:10 +0100

    create vue project

---
+++ lab11/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld msg="Hello Vue!" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  background-color: white;
+}
+</style>
---
+++ lab11/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: black;
+}
+</style>
