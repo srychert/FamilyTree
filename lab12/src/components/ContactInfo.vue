3f5a24a208fc37cbb011e210ac5b12b2b47e1a88
SebastianRychert <sebastian_rychert@wp.pl>
2022-12-27 19:47:12 +0100

    add contact list functionality

--- lab12/src/components/ContactInfo.vue
+++ lab12/src/components/ContactInfo.vue
@@ -1,34 +1,44 @@
 <template>
-  <li>
-    <h2>{{ name }} <img class="friendIcon" src="friends.png" alt=""></h2>
-    <button>Usuń/Dodaj ze/do znajomych</button>
-    <button>Ukryj/Pokaż szczegóły</button>
-    <ul>
-      <li>
-        <strong>telefon:</strong>
-        {{ phoneNumber }}
-      </li>
-      <li>
-        <strong>e-mail:</strong>
-        {{ emailAddress }}
-      </li>
-    </ul>
-  </li>
+	<li>
+		<h2>{{ name }} <img v-if="isFriend" class="friendIcon" src="friends.png" alt="" /></h2>
+		<button @click="toggleFriend">Usuń/Dodaj ze/do znajomych</button>
+		<button @click="toggleDetails">Ukryj/Pokaż szczegóły</button>
+		<ul v-show="showDetails">
+			<li>
+				<strong>telefon:</strong>
+				{{ phoneNumber }}
+			</li>
+			<li>
+				<strong>e-mail:</strong>
+				{{ emailAddress }}
+			</li>
+		</ul>
+	</li>
 </template>
 
 <script>
 export default {
-  props: ['name', 'phoneNumber', 'emailAddress', 'friend'],
-  data() {
-    return {
-    };
-  },
+	props: ["name", "phoneNumber", "emailAddress", "friend"],
+	data() {
+		return {
+			showDetails: false,
+			isFriend: this.friend,
+		};
+	},
+	methods: {
+		toggleDetails() {
+			this.showDetails = !this.showDetails;
+		},
+		toggleFriend() {
+			this.isFriend = !this.isFriend;
+		},
+	},
 };
 </script>
 
 <style>
-  .friendIcon {
-    margin-left: 10px;
-    width: 30px;
-  }
+.friendIcon {
+	margin-left: 10px;
+	width: 30px;
+}
 </style>
