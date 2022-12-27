308675abd24d31235d70c9b446793c9e663b832b
SebastianRychert <sebastian_rychert@wp.pl>
2022-12-27 19:30:53 +0100

    lab12 szkielet

---
+++ lab12/src/components/ContactInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ name }} <img class="friendIcon" src="friends.png" alt=""></h2>
+    <button>Usuń/Dodaj ze/do znajomych</button>
+    <button>Ukryj/Pokaż szczegóły</button>
+    <ul>
+      <li>
+        <strong>telefon:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>e-mail:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['name', 'phoneNumber', 'emailAddress', 'friend'],
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+  .friendIcon {
+    margin-left: 10px;
+    width: 30px;
+  }
+</style>
