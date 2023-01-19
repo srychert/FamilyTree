79c17aa63b7c4c7f70fb1d3f17f6743a7934c321
SebastianRychert <sebastian_rychert@wp.pl>
2023-01-19 22:56:58 +0100

    add withCredentials

--- projekt/frontend/src/api.js
+++ projekt/frontend/src/api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 export default () => {
-  const instance = axios.create({
-    baseURL: "http://localhost:5000/",
-    timeout: 1000,
-    headers: { "Content-Type": "application/json" },
-  });
+	const instance = axios.create({
+		baseURL: "http://localhost:5000/",
+		timeout: 1000,
+		headers: { "Content-Type": "application/json" },
+		withCredentials: true,
+	});
 
-  return instance;
+	return instance;
 };
