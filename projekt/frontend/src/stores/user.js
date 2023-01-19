64a8e3c0209ea30f3d335c724366f1e6565af889
SebastianRychert <sebastian_rychert@wp.pl>
2023-01-19 22:57:42 +0100

    user store

---
+++ projekt/frontend/src/stores/user.js
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+import api from "../api";
+import Cookies from "js-cookie";
+
+export const useUserStore = defineStore("user", {
+	state: () => ({
+		user: null,
+	}),
+
+	actions: {
+		async logIn(login, password) {
+			const res = await api().post("/login", {
+				login,
+				password,
+			});
+
+			const user = res.data;
+			Cookies.set("userId", user.id);
+			this.user = user;
+		},
+		async logOut() {
+			await api().post("/logout");
+			Cookies.remove("userId");
+			this.user = null;
+		},
+		async getUser(id) {
+			const res = await api().get(`/users/${id}`);
+			const user = res.data;
+
+			this.user = user;
+		},
+	},
+});
