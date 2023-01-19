4a3bc4ff896e447466c166c9b78368a97ae9e350
SebastianRychert <sebastian_rychert@wp.pl>
2023-01-19 22:58:15 +0100

    auto fetch user

--- projekt/frontend/src/App.vue
+++ projekt/frontend/src/App.vue
@@ -1,11 +1,26 @@
 <script setup>
 import SideBar from "@/components/SideBar.vue";
 import MainContent from "@/components/MainContent.vue";
+import { useUserStore } from "@/stores/user";
+import { useRouter } from "vue-router";
+import Cookies from "js-cookie";
+
+const userStore = useUserStore();
+const router = useRouter();
+
+if (!userStore.user) {
+	const userId = Cookies.get("userId");
+	if (userId) {
+		userStore.getUser(userId).catch((err) => console.error(err));
+	} else {
+		router.push("/login");
+	}
+}
 </script>
 
 <template>
-  <SideBar></SideBar>
-  <MainContent />
+	<SideBar></SideBar>
+	<MainContent />
 </template>
 
 <style scoped></style>
