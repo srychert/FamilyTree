b4fbf8587f580e02ad3f2b059b529749a3f28892
SebastianRychert <sebastian_rychert@wp.pl>
2023-01-19 20:29:00 +0100

    fix cors and login

--- projekt/backend/src/index.js
+++ projekt/backend/src/index.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 // load .env
 require("dotenv").config();
 
-app.use(cors());
+const corsOptions = {
+	origin: true, //To allow requests from client
+	credentials: true,
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(require("body-parser").json());
--- projekt/backend/src/routes/auth.js
+++ projekt/backend/src/routes/auth.js
@@ -10,7 +10,12 @@ router.post(
 		return res.status(401).send();
 	},
 	function (req, res, _next) {
-		return res.status(200).send({ id: req.user._id });
+		return res.status(200).send({
+			id: req.user._id,
+			login: req.user.login,
+			registrationDate: req.user.registrationDate,
+			role: req.user.role
+		});
 	}
 );
 
