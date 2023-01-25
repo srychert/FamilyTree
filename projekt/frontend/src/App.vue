d2d6e090bc2dc05c85d46d7901f03f47f087cd21
SebastianRychert <sebastian_rychert@wp.pl>
2023-01-25 10:53:03 +0100

    better routes handling

--- projekt/frontend/src/App.vue
+++ projekt/frontend/src/App.vue
@@ -1,15 +1,6 @@
 <script setup>
 import SideBar from "@/components/SideBar.vue";
 import MainContent from "@/components/MainContent.vue";
-import { useUserStore } from "@/stores/user";
-import Cookies from "js-cookie";
-
-const userStore = useUserStore();
-const userId = Cookies.get("userId");
-
-if (!userStore.user && userId) {
-	userStore.getUser(userId).catch((err) => console.error(err));
-}
 </script>
 
 <template>
--- projekt/frontend/src/router/index.js
+++ projekt/frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Cookies from "js-cookie";
+import { useUserStore } from "@/stores/user";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,15 +38,27 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from) => {
+	// allowed routes for not logedIn users
 	if (["login", "register"].includes(to.name)) return true;
 
 	const userId = Cookies.get("userId");
+	const userStore = useUserStore();
 
-	if (userId) return true;
+	if (!userStore.user && userId) {
+		userStore.getUser(userId).catch((err) => {
+			if (err.response.status === 401) {
+				Cookies.remove("userId");
+				router.push("/login");
+				return false;
+			} else {
+				console.error(err);
+				// maybe push to error page?
+				return false;
+			}
+		});
+	}
 
-	router.push("/login");
-	//  return false to cancel the navigation
-	return false;
+	return true;
 });
 
 export default router;
